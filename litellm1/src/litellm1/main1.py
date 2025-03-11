7d4df93751fafdbfc50285be5b5d2fca3b826a34
maryamamin12 <bamin0204@gmail.com>
2025-03-11 21:39:25 +0500

    litellm in main1

--- litellm1/src/litellm1/main1.py
+++ litellm1/src/litellm1/main1.py
@@ -1,4 +1,23 @@
-from crews.flow import Flow, listen, start
+from crewai.flow import Flow, listen, start
 from litellm import completion
 
 
+
+class litellm1(Flow):
+    @start()
+    def start_function(self):
+        output = completion(
+            model = "gemini/gemini-1.5-flash",
+            messages=[
+                {
+                "role": "user",
+                "content": "who is the founder of pakistan"
+            }
+            
+        ])
+        return output['choices'][0]['message']['content']
+
+def run_litellm_flow():
+    flow = litellm1()
+    result = flow.kickoff()
+    print(result)
