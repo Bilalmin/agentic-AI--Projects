f284c7566a61520003aa26d07321c140f84801fc
maryamamin12 <bamin0204@gmail.com>
2025-06-18 00:05:36 +0500

    basic agent

---
+++ openai_sdk/01_agents/basic_agent.py
@@ -0,0 +1,27 @@
+from agents import Agent, Runner, OpenAIChatCompletionsModel,set_tracing_disabled
+from openai import AsyncClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+set_tracing_disabled(disabled=True)
+
+google_api_key = os.getenv("GEMINI_API_KEY")
+
+
+client = AsyncClient(
+    api_key=google_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    
+    
+)
+    
+agent = Agent(
+    name="Assistant", 
+    instructions="You are a helpful assistant",
+    model=OpenAIChatCompletionsModel(model="gemini-2.0-flash",openai_client=client)
+)
+
+query = input("enter your query:")
+result = Runner.run_sync(agent, query)
+print(result.final_output)
