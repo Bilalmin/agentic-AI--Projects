609cf65c85630bb70c0a9556270c6ecbf47745b9
maryamamin12 <bamin0204@gmail.com>
2025-06-17 21:54:51 +0500

    crewai

--- hello-fastapi2/test_main.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
-def test_hello_message():
-    response = client.get("/chat")
-    assert response.status_code == 200
-    assert response.json() == {"message": "online class data science"}
